9c429749f27950da5c01e5328aabbc275d7118ab
Henri Kiiski <henri.kiiski@live.com>
2019-01-28 16:00:54 +0200

    Vaihdettu kaikki komennot menemään commandsHandlerin kautta ja jokaisella komennolla on nyt oma cd

--- mobile.py
+++ mobile.py
@@ -18,32 +18,54 @@ class TelegramBot:
         dispatcher.add_handler(CommandHandler('wabu', self.vappu))
         dispatcher.add_handler(CommandHandler('sekseli', self.sekseli))
 
+        self.viim_kom = {'vappu': [], 'kiitos': [], 'sekseli': []}
+
         updater.start_polling()
         updater.idle()
 
     def vappu(self, bot, update):
         if not self.aikaTarkistus(update.message.date):
             return
+        if not self.viimeKomentoTarkistus('vappu', update):
+            return
         wabu = date(2019, 4, 15)
         tanaan = date.today()
         erotus = wabu - tanaan
         bot.send_message(chat_id=update.message.chat_id,
-                         text='Wabun alkuun on {} paivaa'.format(erotus.days))
+                         text='Wabun alkuun on {} päivää'.format(erotus.days))
 
     def kiitos(self, bot, update):
-        if self.aikaTarkistus(update.message.date):
-            bot.send_message(chat_id=update.message.chat_id, text='Kiitos Jori')
+        if not self.aikaTarkistus(update.message.date):
+            return
+        if not self.viimeKomentoTarkistus('kiitos', update):
+            return
+        bot.send_message(chat_id=update.message.chat_id, text='Kiitos Jori')
 
     def sekseli(self, bot, update):
-        if self.aikaTarkistus(update.message.date):
-            bot.send_message(chat_id=update.message.chat_id, text='Akseli sekseli guu nu kaijakka niko toivio sitä r el'
+        if not self.aikaTarkistus(update.message.date):
+            return
+        if not self.viimeKomentoTarkistus('sekseli', update):
+            return
+        bot.send_message(chat_id=update.message.chat_id, text='Akseli sekseli guu nu kaijakka niko toivio sitä r el'
                                                                   'sa')
 
     def aikaTarkistus(self, viesti_aika):
-        if datetime.today() - viesti_aika < timedelta(0, 10):
+        if datetime.today() - viesti_aika < timedelta(0, 30):
             return True
         else:
             return False
+
+    def viimeKomentoTarkistus(self, komento, update, sekunnit=5):
+        for msg in self.viim_kom[komento]:
+            if msg.message.chat_id == update.message.chat_id:
+                if datetime.today() - msg.message.date > timedelta(0, sekunnit):
+                    self.viim_kom[komento].remove(msg)
+                    self.viim_kom[komento].append(update)
+                    return True
+                else:
+                    return False
+        self.viim_kom[komento].append(update)
+        return True
 
 
 if __name__ == '__main__':
