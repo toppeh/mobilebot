096b4f13a644499e0178afc5beb7a8576968d1e6
Henri Kiiski <henri.kiiski@live.com>
2019-01-28 12:29:34 +0200

    Lisätty 30s ''tuoreus''vaatimus komennoille ja .gitignore

--- mobile.py
+++ mobile.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
-from datetime import date, timedelta
+from datetime import date, timedelta, datetime
 import logging
 from config import TOKEN
 
@@ -22,6 +22,8 @@ class TelegramBot:
         updater.idle()
 
     def vappu(self, bot, update):
+        if not self.aikaTarkistus(update.message.date):
+            return
         wabu = date(2019, 4, 15)
         tanaan = date.today()
         erotus = wabu - tanaan
@@ -29,13 +31,21 @@ class TelegramBot:
                          text='Wabun alkuun on {} paivaa'.format(erotus.days))
 
     def kiitos(self, bot, update):
-        bot.send_message(chat_id=update.message.chat_id, text='Kiitos Jori')
+        if self.aikaTarkistus(update.message.date):
+            bot.send_message(chat_id=update.message.chat_id, text='Kiitos Jori')
 
     def sekseli(self, bot, update):
-        bot.send_message(chat_id=update.message.chat_id, text='Akseli sekseli guu nu kaijakka niko toivio sitä r elsa')
+        if self.aikaTarkistus(update.message.date):
+            bot.send_message(chat_id=update.message.chat_id, text='Akseli sekseli guu nu kaijakka niko toivio sitä r el'
+                                                                  'sa')
+
+    def aikaTarkistus(self, viesti_aika):
+        if datetime.today() - viesti_aika < timedelta(0, 10):
+            return True
+        else:
+            return False
 
 
 if __name__ == '__main__':
     TelegramBot()
-
 
