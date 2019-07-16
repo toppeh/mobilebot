7c478c540e7dc01e9e1a6cb9ba7a25f95908478a
toppeh <38320587+toppeh@users.noreply.github.com>
2019-07-16 20:41:57 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,14 @@
+TOKEN = 'telegram_bot_token'
+TOKEN_KB = 'another_tg_bot_for_testing'
+MOBILE_ID = 000000000000 # tg chat id
+DB_FILE = 'mobile.db'
+DARKSKYTOKEN = "darkskytoken"
+MEMBERS = ["1.", "2."]
+
+SPIRITS = ('Jaloviinaa*', 'Jaloviinaa**', 'Jaloviinaa***', 'Vergiä', 'vodkaa', 'tequilaa', 'giniä', 'Bacardia',
+           'martinia', 'absinttia', 'punaviiniä', 'valkoviiniä', 'Jägermeisteria', 'viskiä', 'salmiakkikossua',
+           'rommia', 'konjakkia', 'Baileys', 'Gambinaa', 'Carilloa', 'Valdemaria', '???')
+MIXERS = ('olutta', 'kiljua', 'glögiä', 'vettä', 'Coca-Colaa', 'energiajuomaa', 'lonkeroa', 'Spriteä', 'maitoa',
+          'kahvia', 'kuohuviiniä', 'shamppanjaa', 'pontikkaa', 'simaa', 'sangriaa', 'tonic-vettä', 'siideriä',
+          'roséviiniä', 'bensaa', 'kirsikkamehua', 'ananasmehua', 'appelsiinimehua', 'omenamehua', 'mitä tahansa',
+          'piimää', 'Muumi-limpparia', 'extra virgin -oliiviöljyä')
